7cb56f917765876b9ee3778d9823c31e01f3f548
Bedagya-Bordoloi <157042250+Bedagya-Bordoloi@users.noreply.github.com>
2026-02-20 01:54:05 +0530

    Delete agentic_ai directory

--- agentic_ai/correction/termination.py
+++
@@ -1,3 +0,0 @@
-class TerminationController:
-    def should_terminate(self, action: str) -> bool:
-        return action in ["accept", "stop_max_retries"]
--- agentic_ai/test.py
+++
@@ -1,9 +0,0 @@
-# test_model.py
-from config import client, MODEL_NAME
-
-response = client.models.generate_content(
-    model=MODEL_NAME,
-    contents="Say 'API is working' if you can read this."
-)
-
-print(response.text.strip())
